bc331aa41480b5306c3f2239c5ed9a676efbab2e
wuxmax <mail@max-reinhard.de>
2022-02-09 18:08:35 +0100

    added 3-way comparison histograms, simra outlier filtering, simulation script

--- adsp/parameter_evaluation/comparison_utils.py
+++ adsp/parameter_evaluation/comparison_utils.py
@@ -1,19 +1,28 @@
 import imp
+from re import I
 import sys
 sys.path.extend(['..', '../..'])
 
-from typing import Tuple, Dict
+from typing import Tuple, Dict, List
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import ticker
 from shapely.geometry import box, Point
+import contextily as cx
 
 from db_utils import get_rect_to_rect_data
 
 # Disable SettingWithCopyWarning
 pd.options.mode.chained_assignment = None  # default='warn'
+
+# use this style for better visibility in histograms
+plt.style.use('seaborn-white')
+plt_kwargs = dict(histtype='stepfilled', alpha=0.3, density=True, ec="k")
+
+COLORS = ['blue', 'orange', 'green']
 
 def analyze_and_plot_ride_data(ride_data: Dict[str, float]):
     print("Ride paths:")
@@ -25,25 +34,29 @@ def analyze_and_plot_ride_data(ride_data: Dict[str, float]):
     print("Ride duration histograms (normalized):")
     calculate_and_plot_ride_durations(ride_data)
 
+def get_hist_bins(data: List[List[float]], binwidth: float):
+    data_flattened =  [item for sublist in data for item in sublist]
+    return np.arange(min(data_flattened), max(data_flattened) + binwidth, binwidth)
+
 def plot_velocity_histograms(ride_data: Dict[str, pd.DataFrame]):
-    ax_simra = ride_data['SimRa'].velo.hist(density=True, color='blue', alpha=0.5)
-    ax_sumo = ride_data['SUMO'].velo.hist(density=True, color='orange', alpha=0.5)
-    for ax in [ax_simra, ax_sumo]:
-        ax.set_ylabel("velocity")
+    hist_bins = get_hist_bins([list(d.velo) for d in ride_data.values()], binwidth=0.5)
+    for idx, (ride_data_name, ride_data) in enumerate(ride_data.items()):
+        ax = ride_data.velo.hist(color=COLORS[idx], bins=hist_bins, label=ride_data_name, **plt_kwargs)
+    ax.set_xlabel("velocity (in m/s)")
+    ax.set_ylabel("histogram normalization")
+    plt.legend()
     plt.show()
 
 def plot_ride_paths(ride_data: Dict[str, pd.DataFrame]):
     fig, ax = plt.subplots(figsize=(12, 12))
     ax.set_aspect(1.5)
     
-    colors = ['blue', 'orange']
-
     # plot rides for each dataframe
     for data_idx, (data_name, df) in enumerate(ride_data.items()):
         df_grouped = df.groupby('ride_id')
         for ride_group_name in df_grouped.groups:
             df_ride_group = df_grouped.get_group(ride_group_name)
-            ax.plot(df_ride_group.lon, df_ride_group.lat, color=colors[data_idx], label=data_name, linewidth=1)
+            ax.plot(df_ride_group.lon, df_ride_group.lat, color=COLORS[data_idx], label=data_name, linewidth=1)
             # add labels to legend
 
     ax.xaxis.set_major_locator(ticker.LinearLocator(4))
@@ -53,41 +66,81 @@ def plot_ride_paths(ride_data: Dict[str, pd.DataFrame]):
     ax.set_xlabel('Longitude in decimal degrees')
     ax.set_ylabel('Latitude in decimal degrees')
 
+    # cx.add_basemap(ax, crs='EPSG:4326', source=cx.providers.Stamen.Toner)
+
     # plt.savefig('simra_vs_sumo_ride_path.png', transparent=True)
     # plt.legend()
     plt.show()
 
-
 def calculate_and_plot_ride_durations(ride_data: Dict[str, pd.DataFrame]):
-    sumo_path_durations = list(ride_data['SUMO'].groupby('ride_id').ts.agg(np.ptp))
     simra_path_durations = [td.total_seconds() for td in ride_data['SimRa'].groupby('ride_id').first().duration]
+    meta_path_durations = {'SimRa': simra_path_durations}
 
+    for ride_data_name in [k for k in ride_data.keys() if k.startswith('SUMO_')]:
+        meta_path_durations[ride_data_name] = list(ride_data[ride_data_name].groupby('ride_id').ts.agg(np.ptp))
+    
     _, ax = plt.subplots()
-    ax.hist(sumo_path_durations, color='orange', alpha=0.5, density=True, bins=10)
-    ax.hist(simra_path_durations, color='blue', alpha=0.5, density=True, bins=60)
-    ax.set_ylabel("duration")
-    plt.xlim(0, 100)
+    hist_bins = get_hist_bins(list(meta_path_durations.values()), binwidth=5)
+    for idx, (ride_data_name, path_durations) in enumerate(meta_path_durations.items()):
+        ax.hist(path_durations, color=COLORS[idx], bins=hist_bins, label=ride_data_name, **plt_kwargs)   
+    ax.set_xlabel("duration (in s)")
+    ax.set_ylabel("histogram normalization")
+    plt.legend()
+    plt.grid()
     plt.show()
 
 
+def get_ride_data(sumo_sim_data_folder: str, scenario_name: str, sumo_sim_files: List[str], 
+        start_rect_coords: Tuple[float, float], end_rect_coords: Tuple[float, float], **kwargs):
 
-def get_ride_data(sumo_sim_file: str, start_rect_coords: Tuple[float, float], end_rect_coords: Tuple[float, float]):
+    sim_data_folder_path = Path(sumo_sim_data_folder)
+    
     print("SimRa:")
-    df_simra = get_rect_to_rect_data(start_rect_coords, end_rect_coords)
+    print(f"color: {COLORS[0]}")
+    df_simra = remove_simra_distance_outliers(get_rect_to_rect_data(start_rect_coords, end_rect_coords))
     df_simra_paths = df_simra[['filename', 'ts', 'lon', 'lat', 'velo', 'time_diff']]
     df_simra_paths.rename({'filename': 'ride_id', 'time_diff': 'duration'}, axis='columns', inplace=True)
 
-    print("SUMO:")
-    df_sumo = pd.read_csv(sumo_sim_file, delimiter=';')
-    df_sumo_paths = df_sumo[['vehicle_id', 'timestep_time', 'vehicle_x', 'vehicle_y', 'vehicle_speed']]
-    df_sumo_paths.rename({'vehicle_id': 'ride_id', 'timestep_time': 'ts', 'vehicle_x': 'lon', 'vehicle_y': 'lat', 'vehicle_speed': 'velo'}, axis='columns', inplace=True)
-    
-    df_sumo_paths_grouped = df_sumo_paths.groupby('ride_id')
-    print(f"Number of rides: {len(df_sumo_paths_grouped)}")
-    for _, ride_group in df_sumo_paths_grouped:
-        df_sumo_paths.drop(index=get_indices_to_delete(ride_group, start_rect_coords, end_rect_coords), inplace=True)
+    ride_data = {'SimRa': df_simra_paths}
 
-    return {'SimRa': df_simra_paths, 'SUMO': df_sumo_paths}
+    for idx, sumo_sim_file in enumerate(sumo_sim_files):
+        print(f"SUMO [{sumo_sim_file}]:")
+        print(f"color: {COLORS[idx + 1]}")
+        df_sumo = pd.read_csv(sim_data_folder_path / sumo_sim_file, delimiter=';')
+        df_sumo_paths = df_sumo[['vehicle_id', 'timestep_time', 'vehicle_x', 'vehicle_y', 'vehicle_speed']]
+        df_sumo_paths.rename({'vehicle_id': 'ride_id', 'timestep_time': 'ts', 'vehicle_x': 'lon', 'vehicle_y': 'lat', 'vehicle_speed': 'velo'}, axis='columns', inplace=True)
+    
+        df_sumo_paths_grouped = df_sumo_paths.groupby('ride_id')
+        print(f"Number of rides: {len(df_sumo_paths_grouped)}")
+        for _, ride_group in df_sumo_paths_grouped:
+            df_sumo_paths.drop(index=get_indices_to_delete(ride_group, start_rect_coords, end_rect_coords), inplace=True)
+        
+        ride_data_name = f"SUMO_{sumo_sim_file[:-len('.csv')]}"
+        ride_data[ride_data_name] = df_sumo_paths
+
+    return ride_data
+
+def remove_simra_distance_outliers(df_simra: pd.DataFrame, iqr_factor: int = 6, verbose: bool = False):
+    df_simra_grouped = df_simra.groupby('filename').agg({'dist': 'sum'})
+
+    # Computing IQR
+    Q1 = df_simra_grouped['dist'].quantile(0.25)
+    Q3 = df_simra_grouped['dist'].quantile(0.75)
+    IQR = Q3 - Q1
+    
+    # Filter by multiple of IQR
+    lower_bound = Q1 - iqr_factor * IQR
+    upper_bound = Q3 + iqr_factor * IQR
+    outlier_filenames = list(df_simra_grouped.query('dist < @lower_bound | dist > @upper_bound').index)
+    df_filtered = df_simra[~df_simra.filename.isin(outlier_filenames)]
+
+    if verbose:
+        df_simra_grouped.hist()
+        print(f"lowerbound: {lower_bound}, upper_bound: {upper_bound}")
+        print(f"n_rides unfiltered: {df_simra_grouped.size}, n_rides filtered: {len(df_filtered.groupby('filename').groups)}")
+  
+    return df_filtered
+
 
 def get_indices_to_delete(ride_group, start_rect_coords: Tuple[float, float], end_rect_coords: Tuple[float, float]):
     start_rect = box(*start_rect_coords)
--- adsp/parameter_evaluation/run_simulation.sh
+++ adsp/parameter_evaluation/run_simulation.sh
@@ -1,18 +1,39 @@
 #! /usr/bin/env bash
 
-SCENARIO_SUB_FOLDER="$1"
-SCENARIO_NAME="$2"
-
 SCENARIO_FOLDER="sim_scenarios"
 SIM_DATA_FOLDER="sim_data"
 
-mkdir tmp_sim
+SCENARIO_NAME_SUFFIXES=("default" "new_params")
 
-/sumo/bin/sumo -c "$SCENARIO_FOLDER"/"$SCENARIO_SUB_FOLDER"/"$SCENARIO_NAME".sumocfg \
-    --fcd-output tmp_sim/fcd_out.xml --device.fcd.explicit vehDist --fcd-output.geo 
+run_simulation() {
+    mkdir tmp_sim
 
-python /sumo/tools/xml/xml2csv.py tmp_sim/fcd_out.xml
+    /sumo/bin/sumo -c "$SCENARIO_FOLDER"/"$SCENARIO_SUB_FOLDER"/"$SCENARIO_NAME".sumocfg \
+        --fcd-output tmp_sim/fcd_out.xml --device.fcd.explicit vehDist --fcd-output.geo 
 
-mv tmp_sim/fcd_out.csv "$SIM_DATA_FOLDER"/"$SCENARIO_NAME".csv
+    python /sumo/tools/xml/xml2csv.py tmp_sim/fcd_out.xml
 
-rm -rf tmp_sim
+    mv tmp_sim/fcd_out.csv "$SIM_DATA_FOLDER"/"$SCENARIO_NAME".csv
+
+    rm -rf tmp_sim
+}
+
+if [[ "$1" == "ALL" ]]; then
+    cd "$SCENARIO_FOLDER"
+    for SUB_FOLDER in */ ; do
+        SCENARIO_SUB_FOLDER=${SUB_FOLDER%/}
+        cd ..
+        for SUFFIX in "${SCENARIO_NAME_SUFFIXES[@]}"; do
+            SCENARIO_NAME="$SCENARIO_SUB_FOLDER"_"$SUFFIX"
+            echo "Running scenario "$SCENARIO_NAME"..."
+            run_simulation
+        done
+    done
+else
+    SCENARIO_SUB_FOLDER="$1"
+    SCENARIO_NAME_SUFFIX="$2"
+    SCENARIO_NAME="$SCENARIO_SUB_FOLDER"_"$SCENARIO_NAME_SUFFIX"
+    echo "Running scenario "$SCENARIO_NAME"..."
+    run_simulation
+fi
+
